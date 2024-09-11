5c96dc60fcfd05c1f2a31e20dfe892d73e4707f4
FernandoNando2 <ferhdom@gmail.com>
2024-09-10 18:33:08 -0600

    Creation of the project

---
+++ src/main/java/com/itq/forjametalica/ForjametalicaApplication.java
@@ -0,0 +1,13 @@
+package com.itq.forjametalica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForjametalicaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForjametalicaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/itq/forjametalica/ForjametalicaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itq.forjametalica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForjametalicaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
